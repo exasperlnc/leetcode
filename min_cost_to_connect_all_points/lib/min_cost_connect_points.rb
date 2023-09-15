27a35e362938046d0f9b8a4ed8824ed2b08b8b7e
Logan Cole <emtlogancole@gmail.com>
2023-09-15 12:50:26 -0500

    grab solution using unionfind

--- min_cost_to_connect_all_points/lib/min_cost_connect_points.rb
+++ min_cost_to_connect_all_points/lib/min_cost_connect_points.rb
@@ -1,19 +1,54 @@
-def min_cost_connect_points(points)
-  # iterate through points
-  array_size = points.count
-  edges = []
+def find(u, parent)
+  return u if parent[u] == u
+  parent[u] = find(parent[u], parent) # Path compression
+  return parent[u]
+end
 
-  for i in 0..(array_size - 1)
-    for j in (i + 1)...array_size 
-      # calculate manhattan distance between each point
+def union(u, v, parent, rank)
+  u = find(u, parent)
+  v = find(v, parent)
+
+  return if u == v # They're already in the same set
+
+  if rank[u] > rank[v]
+    parent[v] = u
+  elsif rank[u] < rank[v]
+    parent[u] = v
+  else
+    parent[v] = u
+    rank[u] += 1
+  end
+end
+
+def min_cost_connect_points(points)
+  edges = []
+  n = points.size
+
+  # Step 1: Calculate all edges and distances
+  (0...n).each do |i|
+    ((i + 1)...n).each do |j|
       distance = (points[i][0] - points[j][0]).abs + (points[i][1] - points[j][1]).abs
       edges << { point_1: i, point_2: j, distance: distance }
     end
   end
 
-  edges.sort! {|edge_1, edge_2| edge_1[:distance] <=> edge_2[:distance] }
-  # .min the result
+  # Step 2: Sort edges by distance
+  edges.sort! { |a, b| a[:distance] <=> b[:distance] }
+
+  # Step 3: Initialize Union-Find data structures
+  parent = Array.new(n) { |i| i }
+  rank = Array.new(n, 0)
+
+  # Step 4: Kruskal's Algorithm
+  min_cost = 0
+  edges.each do |edge|
+    u, v, w = edge[:point_1], edge[:point_2], edge[:distance]
+
+    if find(u, parent) != find(v, parent)
+      min_cost += w
+      union(u, v, parent, rank)
+    end
+  end
+
+  return min_cost
 end
-# dp = Array.new(2^array_size) { Array.new(array_size, Float::INFINITY) }
-# dp[1][0] = 0
-# for i in 
--- min_cost_to_connect_all_points/spec/min_cost_connect_points_spec.rb
+++ min_cost_to_connect_all_points/spec/min_cost_connect_points_spec.rb
@@ -1,2 +1,19 @@
 require './lib/min_cost_connect_points'
 require 'rspec'
+
+describe 'test_cases' do 
+  before :each do
+    @points_1 = [[0,0],[2,2],[3,10],[5,2],[7,0]] #solution: 20
+    @points_2 = [[3,12],[-2,5],[-4,1]] #solution: 18
+  end
+  it 'tests points_1' do
+    result = min_cost_connect_points(@points_1)
+
+    expect(result).to eq(20)
+  end
+  it 'tests_points_2' do
+    result = min_cost_connect_points(@points_2)
+
+    expect(result).to eq(18)
+  end
+end
