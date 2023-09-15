e0eb7d089d99ecf69bc6d4232743f5f0757e6d74
Logan Cole <emtlogancole@gmail.com>
2023-09-15 11:45:05 -0500

    calculate distances between all points

--- min_cost_to_connect_all_points/lib/min_cost_connect_points.rb
+++ min_cost_to_connect_all_points/lib/min_cost_connect_points.rb
@@ -1,7 +1,15 @@
 def min_cost_connect_points(points)
   # iterate through points
-  
-  # create a dag with all possible paths to hit each point once, using DP
+  array_size = points.count
+  distances = Array.new(array_size) { Array.new(array_size, 0) }
 
+  for i in 0..(array_size - 1)
+    for j in (i + 1)...array_size 
+      # calculate manhattan distance between each point
+      distance = (points[i][0] - points[j][0]).abs + (points[i][1] - points[j][1]).abs
+      distances[i][j] = distance
+      distances[j][i] = distance 
+    end
+  end
   # .min the result
 end
