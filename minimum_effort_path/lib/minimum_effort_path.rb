39349855ec354b9be75b6217e972271c48264cb6
Logan Cole <emtlogancole@gmail.com>
2023-09-16 09:58:06 -0500

    psuedocode minimum effort path

---
+++ minimum_effort_path/lib/minimum_effort_path.rb
@@ -0,0 +1,10 @@
+def minimum_effort_path(heights)
+  # iterate through heights
+
+  # save all possible paths, and keep the max_dif saved with each path
+
+  # sort the paths by smallest max_dif
+
+  # return paths[0].max_dif
+  
+end
