f06421b248e71502907da8e48c5dec86a343634b
Logan Cole <emtlogancole@gmail.com>
2023-09-19 13:17:55 -0500

    test: group_anagrams

---
+++ group_anagrams/spec/group_anagrams_spec.rb
@@ -0,0 +1,27 @@
+require './lib/group_anagrams'
+require 'rspec'
+
+describe 'example' do
+  before :each do
+    @in_1 = ["eat","tea","tan","ate","nat","bat"]
+    @out_1 = [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+    @in_2 = [""]
+    @out_2 = [[""]]
+
+    @in_3 = ["a"]
+    @out_3 = [["a"]]
+  end
+
+  it '1' do
+    expect(group_anagrams(@in_1)).to eq(@out_1)
+  end
+
+  it '2' do
+    expect(group_anagrams(@in_2)).to eq(@out_2)
+  end
+
+  it '3' do
+    expect(group_anagrams(@in_3)).to eq(@out_3)
+  end
+end
