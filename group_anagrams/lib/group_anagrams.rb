1753dc5bac08be48c593a6ae475ff05f17e8b79c
Logan Cole <emtlogancole@gmail.com>
2023-09-19 14:02:25 -0500

    refactor, remove redundant keys array

--- group_anagrams/lib/group_anagrams.rb
+++ group_anagrams/lib/group_anagrams.rb
@@ -1,6 +1,5 @@
 def group_anagrams(strs)
   groups = {}
-  keys = []
   solution = []
   
   strs.each do |str|
@@ -9,11 +8,9 @@ def group_anagrams(strs)
       groups[str.chars.tally] << str
     else
       groups[str.chars.tally] = [str]
-      # save the keys in an array
-      keys << str.chars.tally
     end
   end
-  keys.each do |key|
+  groups.keys.each do |key|
     # iterate through the array, populating another array with the values
     # (there has to be a more elegant way)
     solution << groups[key]
