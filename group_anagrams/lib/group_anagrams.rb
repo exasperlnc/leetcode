372d5dd0ecc86b0d822b557ee711d15699416191
Logan Cole <emtlogancole@gmail.com>
2023-09-19 13:39:57 -0500

    group_anagrams psuedocode

--- group_anagrams/lib/group_anagrams.rb
+++ group_anagrams/lib/group_anagrams.rb
@@ -1,3 +1,7 @@
 def group_anagrams(strs)
-  
+  # use tally enumerable to populate a hash
+  # save the keys in an array
+  # iterate through the array, populating another array with the values
+  # (there has to be a more elegant way)
+  # return solution array
 end
