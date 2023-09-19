f3779f2e3ec6fed02172a223d91d60cb96db66b3
Logan Cole <emtlogancole@gmail.com>
2023-09-19 14:05:35 -0500

    refactor: use group_by instead of hash

--- group_anagrams/lib/group_anagrams.rb
+++ group_anagrams/lib/group_anagrams.rb
@@ -1,20 +1,3 @@
 def group_anagrams(strs)
-  groups = {}
-  solution = []
-  
-  strs.each do |str|
-    # use tally enumerable to populate a hash
-    if groups[str.chars.tally]
-      groups[str.chars.tally] << str
-    else
-      groups[str.chars.tally] = [str]
-    end
-  end
-  groups.keys.each do |key|
-    # iterate through the array, populating another array with the values
-    # (there has to be a more elegant way)
-    solution << groups[key]
-  end
-  # return solution array
-  solution
+  strs.group_by {|str| str.chars.sort}.values
 end
