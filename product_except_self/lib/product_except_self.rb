b3d458a7652e54053edc54b89919d3df8bedf6b3
Logan Cole <emtlogancole@gmail.com>
2023-09-20 13:28:52 -0500

    solve product_except_self

--- product_except_self/lib/product_except_self.rb
+++ product_except_self/lib/product_except_self.rb
@@ -1,20 +1,15 @@
 def product_except_self(nums)
-  # iterate through nums
-  solution_a = []
-  nums.each_with_index do |_, index|
-    # perform operation on items before num
-    if index == 0
-      solution_a << nums[1..-1].inject(:*) 
-    else
-      # perform operation on items after num
-      # shove that into a solution array
-      require 'pry'; binding.pry
-      solution_a << nums[0...index].inject(:*) * nums[index...-1].inject(:*)
-    end
+  output = []
+  total_product = 1
+  nums.each do |num|
+    output << total_product
+    total_product *= num
   end
-  
 
-
-  # return solution array
-  solution_a
+  total_product = 1
+  (nums.length-1).downto(0) do |idx|
+    output[idx] = total_product * output[idx]
+    total_product *= nums[idx]
+  end
+  output
 end
