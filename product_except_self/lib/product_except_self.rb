e004332e54b8ee59d2ff8df79203d3742d61cb8d
Logan Cole <emtlogancole@gmail.com>
2023-09-20 13:05:45 -0500

    first pass, still not working

--- product_except_self/lib/product_except_self.rb
+++ product_except_self/lib/product_except_self.rb
@@ -1,11 +1,20 @@
 def product_except_self(nums)
   # iterate through nums
+  solution_a = []
+  nums.each_with_index do |_, index|
+    # perform operation on items before num
+    if index == 0
+      solution_a << nums[1..-1].inject(:*) 
+    else
+      # perform operation on items after num
+      # shove that into a solution array
+      require 'pry'; binding.pry
+      solution_a << nums[0...index].inject(:*) * nums[index...-1].inject(:*)
+    end
+  end
+  
 
-  # perform operation on items before num
-
-  # perform operation on items after num
-
-  # shove that into a solution array
 
   # return solution array
+  solution_a
 end
