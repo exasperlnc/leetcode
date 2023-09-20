dd2145ecead955d079cb2fc18020767e1b54e1de
Logan Cole <emtlogancole@gmail.com>
2023-09-20 13:41:49 -0500

    is_valid_sudoku psuedocode

---
+++ is_valid_sudoku/lib/is_valid_sudoku.rb
@@ -0,0 +1,9 @@
+def is_valid_sudoku(board)
+  # check each row
+    # return false if any repeats
+  # check each column
+    # return false if repeat
+  # check each 9x9 set
+    # return false if any repeat
+  # return true
+end
