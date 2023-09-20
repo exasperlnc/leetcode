b1cdcc0164215acd646fa23de3ac91aed9ccd3f4
Logan Cole <emtlogancole@gmail.com>
2023-09-20 13:50:03 -0500

    check rows for repeats

--- is_valid_sudoku/lib/is_valid_sudoku.rb
+++ is_valid_sudoku/lib/is_valid_sudoku.rb
@@ -1,9 +1,12 @@
 def is_valid_sudoku(board)
   # check each row
+  board.each do |row|
     # return false if any repeats
+    return false if row.reject { |x| x == '.'}.tally.values.any? { |value| value > 1}
+  end
   # check each column
     # return false if repeat
   # check each 9x9 set
     # return false if any repeat
-  # return true
+  return true
 end
