371ce45cfbb1f239d5929f25eafb5996ad5ca5ef
Logan Cole <emtlogancole@gmail.com>
2023-09-20 12:53:42 -0500

    product except self psuedocode

--- product_except_self/lib/product_except_self.rb
+++ product_except_self/lib/product_except_self.rb
@@ -1,3 +1,11 @@
 def product_except_self(nums)
-  
+  # iterate through nums
+
+  # perform operation on items before num
+
+  # perform operation on items after num
+
+  # shove that into a solution array
+
+  # return solution array
 end
