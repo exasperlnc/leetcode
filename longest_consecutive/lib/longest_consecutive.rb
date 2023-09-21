33abca9b2515fb7090e020ddd5a22f8d1554ca8f
Logan Cole <emtlogancole@gmail.com>
2023-09-21 11:15:31 -0500

    added .uniq to sorted

--- longest_consecutive/lib/longest_consecutive.rb
+++ longest_consecutive/lib/longest_consecutive.rb
@@ -1,6 +1,6 @@
 def longest_consecutive(nums)
   # sort nums
-  sorted = nums.sort
+  sorted = nums.sort.uniq
   # iterate through nums and keep track of current conescutive and max consecutive
   current = 0
   longest = 0
@@ -8,7 +8,6 @@ def longest_consecutive(nums)
     longest = 1 if index == 0
     current = 1 if index == 0
     next if index == 0
-    
     # if the number before is consecutive, current += 1
     if sorted[index-1] - num == -1
       current += 1
