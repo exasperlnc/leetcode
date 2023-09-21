6801af0239edeb30dbf36b68d7e5b4271ec5c312
Logan Cole <emtlogancole@gmail.com>
2023-09-21 10:56:21 -0500

    longest consecutive pseudocode

---
+++ longest_consecutive/lib/longest_conescutive.rb
@@ -0,0 +1,9 @@
+def longest_consecutive(nums)
+  # sort nums
+
+  # iterate through nums and keep track of current conescutive and max consecutive
+
+  # if current consecutive > max consecutive, max consecutive = current consecutive
+  
+  # return max consecutive
+end
