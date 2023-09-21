297912430d53a1ac928ba452956ddd2d751635fe
Logan Cole <emtlogancole@gmail.com>
2023-09-21 11:07:13 -0500

    first pass longest consecutive

--- longest_consecutive/lib/longest_consecutive.rb
+++ longest_consecutive/lib/longest_consecutive.rb
@@ -1,19 +1,24 @@
 def longest_consecutive(nums)
   # sort nums
-  nums.sort!
+  sorted = nums.sort
   # iterate through nums and keep track of current conescutive and max consecutive
   current = 0
   longest = 0
-  nums.each_with_index do |num, index|
+  sorted.each_with_index do |num, index|
     longest = 1 if index == 0
+    current = 1 if index == 0
     next if index == 0
     
     # if the number before is consecutive, current += 1
-
-    # if the number before isn't, current = 1
-
+    if sorted[index-1] - num == -1
+      current += 1
+      longest = current if current > longest
+    else 
+      # if the number before isn't, current = 1
+      current = 1
+    end
     # if current > longest, longest == consecutive 
-
+  end
   # return max consecutive
   return longest
 end
