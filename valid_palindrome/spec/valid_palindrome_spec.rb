4fa01cab01b25b81fa86da3e2619221552b0b0d1
Logan Cole <emtlogancole@gmail.com>
2023-09-22 17:35:26 -0500

    test: valid palindrome

---
+++ valid_palindrome/spec/valid_palindrome_spec.rb
@@ -0,0 +1,27 @@
+require './lib/valid_palindrome'
+require 'rspec'
+
+describe 'example' do
+  before :all do
+    @in_1 = "A man, a plan, a canal: Panama"
+    @out_1 = true
+
+    @in_2 = "race a car"
+    @out_2 = false
+
+    @in_3 = " "
+    @out_3 = true
+  end
+
+  it '1' do
+    expect(valid_palindrome(@in_1)).to eq(@out_1)
+  end
+
+  it '2' do
+    expect(valid_palindrome(@in_2)).to eq(@out_2)
+  end
+
+  it '3' do
+    expect(valid_palindrome(@in_3)).to eq(@out_3)
+  end
+end
