190a8fa725e404d33bd3bcc2b71790bf4f348b94
Logan Cole <emtlogancole@gmail.com>
2023-09-22 17:42:33 -0500

    pseudocode pointer valid palindrome

---
+++ valid_palindrome/lib/pointer_valid_palindrome.rb
@@ -0,0 +1,10 @@
+def pointer_valid_palindrome(s)
+  # define pointers for each index
+
+  # while l_pointer < r_pointer
+
+  # see if the chars are the same
+    # return false if not
+
+  # return true 
+end
