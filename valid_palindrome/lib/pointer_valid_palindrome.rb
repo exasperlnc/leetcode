4d686619942c01f1a31dfb1cb3fa8a9e0a1c247e
Logan Cole <emtlogancole@gmail.com>
2023-09-24 11:54:56 -0500

    fixed pointer: r_pointer changed to decrement instead in increment in while looop

--- valid_palindrome/lib/pointer_valid_palindrome.rb
+++ valid_palindrome/lib/pointer_valid_palindrome.rb
@@ -1,10 +1,17 @@
 def pointer_valid_palindrome(s)
   # define pointers for each index
-
+  check = s.downcase.scan(/[a-z0-9]/).join
+  l_pointer = 0
+  r_pointer = check.length - 1
   # while l_pointer < r_pointer
-
   # see if the chars are the same
-    # return false if not
+  # return false if not
+  while l_pointer < r_pointer
+    return false if check[l_pointer] != check[r_pointer]
+    l_pointer += 1
+    r_pointer -= 1
+  end
 
   # return true 
+  return true
 end
