8b4fb2f2965a2f2e9ba2ecd458415779f7f56c31
Logan Cole <emtlogancole@gmail.com>
2023-09-24 12:19:37 -0500

    two sum pointer

--- two_sum/lib/pointer_two_sum.rb
+++ two_sum/lib/pointer_two_sum.rb
@@ -1,17 +1,28 @@
 def pointer_two_sum(nums, target)
   # save pointers
+  l_index = 0
+  r_index = nums.length - 1
 
   # while l < r
+  while l_index < r_index
     # l spaceship r
+    case nums[l_index] + nums[r_index] <=> target
 
     # when 1 
+    when 1
       # decrement r
+      r_index -= 1
 
     # when 0
+    when 0
       # return indexes
+      return [l_index + 1, r_index + 1]
     
     # when -1 
+    when -1
       # increment l
-
-  # false
+      l_index += 1
+    end
+  end
+  false
 end
--- two_sum/spec/pointer_two_sum_spec.rb
+++ two_sum/spec/pointer_two_sum_spec.rb
@@ -8,23 +8,23 @@ describe 'example' do
     @target_1 = 9
 
     #[1,2]
-    @nums_2 = [3,2,4]
+    @nums_2 = [2,3,4]
     @target_2 = 6
 
     #[0,1]
-    @nums_3 = [3,3]
-    @target_3 = 6
+    @nums_3 = [-1,0]
+    @target_3 = -1
   end
 
   it '1' do
-    expect(pointer_two_sum(@nums_1, @target_1)).to eq([0,1])
+    expect(pointer_two_sum(@nums_1, @target_1)).to eq([1,2])
   end
 
   it '2' do
-    expect(pointer_two_sum(@nums_2, @target_2)).to eq([1,2])
+    expect(pointer_two_sum(@nums_2, @target_2)).to eq([1,3])
   end
 
   it '3' do
-    expect(pointer_two_sum(@nums_3, @target_3)).to eq([0,1])
+    expect(pointer_two_sum(@nums_3, @target_3)).to eq([1,2])
   end
 end
