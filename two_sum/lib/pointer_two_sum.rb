1e747fa53ce46a0c0a3c3ab49d7bd9d0c2e75895
Logan Cole <emtlogancole@gmail.com>
2023-09-24 12:12:47 -0500

    pseudocode pointer two sum

--- two_sum/lib/pointer_two_sum.rb
+++ two_sum/lib/pointer_two_sum.rb
@@ -1,3 +1,17 @@
 def pointer_two_sum(nums, target)
-  
+  # save pointers
+
+  # while l < r
+    # l spaceship r
+
+    # when 1 
+      # decrement r
+
+    # when 0
+      # return indexes
+    
+    # when -1 
+      # increment l
+
+  # false
 end
