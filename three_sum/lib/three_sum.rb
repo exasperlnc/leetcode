bb5d54202f556c1aab4c981c6ff430d192a6b639
Logan Cole <emtlogancole@gmail.com>
2023-09-25 10:02:56 -0500

    pseudocode three sum

--- three_sum/lib/three_sum.rb
+++ three_sum/lib/three_sum.rb
@@ -1,3 +1,21 @@
 def three_sum(nums)
-  
+  # solutions = []
+
+  # sort nums
+
+  # iterate through nums
+
+  # set the goal number as the current num
+
+  # us l_index and r_index
+
+  # compare with spaceship 
+
+  # if 1, decrement r
+
+  # if 0, add all nums to the solutions array 
+
+  # if -1, increment l 
+
+  # return solutions
 end
