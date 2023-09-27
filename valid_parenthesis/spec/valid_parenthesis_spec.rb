f43659c49236fe3208dc83e544101d947ff42553
Logan Cole <emtlogancole@gmail.com>
2023-09-27 14:04:33 -0500

    test: valid parenthesis

---
+++ valid_parenthesis/spec/valid_parenthesis_spec.rb
@@ -0,0 +1,35 @@
+require './lib/valid_parenthesis'
+require 'rspec'
+
+describe 'example' do
+  before :all do
+    @in_1 = "()"
+    @out_1 = true
+
+    @in_2 = "()[]{}"
+    @out_2 = true
+
+    @in_3 = "(]"
+    @out_3 = false
+
+    @in_4 = "([)]"
+    @out_4 = false
+  end
+
+  it '1' do
+    expect(@in_1).to eq(@out_1)
+  end
+
+  it '2' do
+    expect(@in_2).to eq(@out_2)
+    
+  end
+
+  it '3' do
+    expect(@in_3).to eq(@out_3)
+  end
+
+  it '4' do
+    expect(@in_4).to eq(@out_4)
+  end
+end
