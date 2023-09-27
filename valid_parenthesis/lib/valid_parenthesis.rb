e329f910c5e038a14ec7d93cafece14d8ec8252a
Logan Cole <emtlogancole@gmail.com>
2023-09-27 14:08:09 -0500

    pseudocode valid parenthesis

---
+++ valid_parenthesis/lib/valid_parenthesis.rb
@@ -0,0 +1,9 @@
+def valid_parenthesis(string)
+  # add items to an array if they are opener strings
+
+  # pop them off the end and compare them when you hit a closer
+
+  # return false if they don't match
+
+  # return true at the bottom
+end
