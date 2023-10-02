8accb7e78d2186734409e952c21516b0815a0279
Logan Cole <emtlogancole@gmail.com>
2023-10-02 11:47:15 -0500

    second pass, bsearch by hand

--- binary_search/lib/binary_search.rb
+++ binary_search/lib/binary_search.rb
@@ -1,15 +1,22 @@
 def binary_search(nums, target)
+  return -1 if nums.empty? 
   # define l and r 
-
+  l = 0
+  r = nums.length - 1 
   # run a while loop for l <= r
-
+  while l <= r
+    midpoint = (l + r) / 2
   # iterate through the nums array using midpoints and looking for target
-    # if target is bigger than the number at index, increase the pointers
-
-
-    # if the target is smaller, decrease them
-
+  # if target is bigger than the number at index, increase the pointers
+    if nums[midpoint] < target
+      l += 1
+  # if the target is smaller, decrease them
+    elsif nums[midpoint] > target 
+      r -= 1
+    else
+      return midpoint
+    end
     # if the target is the number, return it's index
-
-  # return -1 
+  end
+  return -1 
 end
