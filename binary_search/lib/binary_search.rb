128538c18187972d171b1e5666d56c6821ed9c3a
Logan Cole <emtlogancole@gmail.com>
2023-10-02 11:34:05 -0500

    first pass binary search

--- binary_search/lib/binary_search.rb
+++ binary_search/lib/binary_search.rb
@@ -1,6 +1,7 @@
 def binary_search(nums, target)
   # iterate through nums with bsearch, find target
-
+  search = nums.bsearch_index { |a, index| target <=> a }
+  return search if search 
   # if the bsearch == target, return target 
-  # else, return -1 
+  return -1 
 end
