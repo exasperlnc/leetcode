e5b35ae38640e93356a50490e7b5bc698e325eb0
Logan Cole <emtlogancole@gmail.com>
2023-10-03 14:36:53 -0500

    test: longest substring wihtout repeating

---
+++ longest_substring_without_repeating/spec/length_of_longest_substring_spec.rb
@@ -0,0 +1,20 @@
+require './lib/length_of_longest_substring'
+require 'rspec'
+
+describe 'example' do
+  before :all do
+    @in_1 = "abcabcbb"
+    @out_1 = 3
+
+    @in_2 = "bbbbb"
+    @out_2 = 1
+  end
+
+  it '1' do
+    expect(length_of_longest_substring(@in_1)).to eq(@out_1)
+  end
+
+  it '2' do
+    expect(length_of_longest_substring(@in_2)).to eq(@out_2)
+  end
+end
