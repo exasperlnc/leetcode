a0dd9b2e9be3ac432baa989b311b2b0804c1f61d
Logan Cole <emtlogancole@gmail.com>
2023-10-04 12:51:59 -0500

    clean up solution: store indexes in hash

--- longest_substring_without_repeating/lib/length_of_longest_substring.rb
+++ longest_substring_without_repeating/lib/length_of_longest_substring.rb
@@ -1,45 +1,20 @@
 def length_of_longest_substring(s)
-  # pointer one is last repeat 
-  # pointer two is iterating through string s 
-  # max is the max string 
-  i = 0
-  max = 0
-  string_saver = ''
-  s_chars = s.chars
   hash = {}
-  while i <= s.length - 1
-      letter = s_chars[i]
-      # if its not a repeat, add it to the string
-      if hash[letter] != true
-          string_saver << letter 
-          hash[letter] = true
-      # if it is a repeat, go to wherever the repeat is in the string and restart counting after that 
-      else
-          # go to the first appearance of the letter in the string and reset the string to that 
-          new_string = ''
-          found = false
-          string_saver.chars.each do |char|
-            if !found
-                if char == letter
-                  found = true
-                  hash.clear
-                  new_string << letter 
-                  hash[char] = true
-                end
-              else
-                new_string << char
-                hash[char] = true
-            end
-          end
-          if !new_string.include?(letter)
-            new_string << letter
-          end
-          string_saver = new_string
-      end
-      if string_saver.length > max
-          max = string_saver.length 
-      end
-      i += 1
+  pointer = 0 
+  start = 0
+  max = 0
+
+  while pointer < s.length
+    letter = s[pointer]
+
+    if hash.key?(letter) && hash[letter] >= start
+      start = hash[letter] + 1 
+    end
+
+    hash[letter] = pointer
+
+    max = [max, pointer - start + 1].max
+    pointer += 1 
   end
-  return max
+  max 
 end
