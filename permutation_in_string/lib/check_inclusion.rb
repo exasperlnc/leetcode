06fd3cdeee6e26464f4abe0db71108ce44ad247a
Logan Cole <emtlogancole@gmail.com>
2023-10-04 13:39:09 -0500

    second pass, hashes update in real time

--- permutation_in_string/lib/check_inclusion.rb
+++ permutation_in_string/lib/check_inclusion.rb
@@ -1,15 +1,28 @@
 def check_inclusion(s1, s2)
-  compare = s1.downcase.chars.sort
-
+  compare = s1.chars.tally
+  alphabet_hash = Hash.new(0)
   p1 = 0
   p2 = s1.length - 1
 
+  # populate the initial hash 
+  s2[p1..p2].chars.each {|char| alphabet_hash[char] += 1}
+
   while p2 <= s2.length - 1
-    if compare == s2[p1..p2].downcase.chars.sort
+    if compare == alphabet_hash
       return true
     end
+  
+    p1_letter = s2[p1]
+    alphabet_hash[p1_letter] -= 1
+
+    alphabet_hash.delete(p1_letter) if alphabet_hash[p1_letter] == 0
+    
     p1 += 1
     p2 += 1
+    if p2 < s2.length
+      p2_letter = s2[p2]
+      alphabet_hash[p2_letter] += 1
+    end
   end
-  return false 
+  compare == alphabet_hash
 end
