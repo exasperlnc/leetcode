cd545396ce107a86d6138437d0681958d66b58c7
Logan Cole <emtlogancole@gmail.com>
2023-10-07 15:31:33 -0500

    pseudocode 2d matrix

---
+++ search_2d_matrix/lib/search_matrix.rb
@@ -0,0 +1,10 @@
+def search_matrix(matrix, target)
+  # search the beginning of each array to find one that is greater than or equal to the target 
+
+  # if its equal return true
+  
+  # if its greater search the previous array for the target 
+    #true if found
+
+  # return false
+end
