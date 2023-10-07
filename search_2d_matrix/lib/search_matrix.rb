75674cdbc5ad3a0533f7f7bde38f1c85e41a68a2
Logan Cole <emtlogancole@gmail.com>
2023-10-07 15:51:44 -0500

    retype for memory

--- search_2d_matrix/lib/search_matrix.rb
+++ search_2d_matrix/lib/search_matrix.rb
@@ -1,6 +1,6 @@
 def search_matrix(matrix, target)
   row_index = matrix.map(&:last).bsearch_index {|last| target <= last}
   return false unless row_index
-  cell = matrix[row_index].bsearch {|num,_| target <=> num}
-  return cell ? true : false
+  cell = matrix[row_index].bsearch {|num| target <=> num}
+  cell ? true : false 
 end
