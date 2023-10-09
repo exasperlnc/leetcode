940214a992c5194ab57a1146593519e4df556e83
Logan Cole <emtlogancole@gmail.com>
2023-10-09 15:46:40 -0500

    first pass invert tree test

---
+++ invert_binary_tree/spec/invert_tree_spec.rb
@@ -0,0 +1,27 @@
+require './lib/invert_tree'
+require 'rspec'
+
+describe 'example' do
+  before :all do
+    @tree_1 = TreeNode.new(4)
+    @tree_1.left = TreeNode.new(2)
+    @tree_1.left.left = TreeNode.new(1)
+    @tree_1.left.right = TreeNode.new(3)
+    @tree_1.right = TreeNode.new(7)
+    @tree_1.right.left = TreeNode.new(6)
+    @tree_1.right.right = TreeNode.new(9)
+
+    @out_1 = TreeNode.new(4)
+    @out_1.right = TreeNode.new(2)
+    @out_1.right.right = TreeNode.new(1)
+    @out_1.right.left = TreeNode.new(3)
+    @out_1.left = TreeNode.new(7)
+    @out_1.left.right = TreeNode.new(6)
+    @out_1.left.left = TreeNode.new(9)
+  end
+
+  it '1' do
+    expect(invert_tree(@tree_1.left.val)).to eq(@out_1.right.val)
+    expect(invert_tree(@tree_1.right.val)).to eq(@out_1.left.val)
+  end
+end
