1c92430b0111c54d3dd2010048655f5060ce39a8
Logan Cole <emtlogancole@gmail.com>
2023-11-04 16:24:28 -0500

    test: max depth

--- max_depth_bt/spec/max_depth_spec.rb
+++ max_depth_bt/spec/max_depth_spec.rb
@@ -3,14 +3,21 @@ require './lib/max_depth'
 
 RSpec.describe 'max depth' do
   it '1' do
+    root = [3,9,20,null,null,15,7]
+    root = TreeNode.new(3)
+    root.left = TreeNode.new(9)
+    root.left = TreeNode.new(20)
+    root.right = TreeNode.new(20)
+    root.right.left = TreeNode.new(15)
+    root.right.right = TreeNode.new(7)
     
+    expect(max_depth(root)).to eq(3)
   end
 
   it '2' do
+    root = TreeNode.new(1)
+    root.right = TreeNode.new(2)
     
-  end
-
-  it '3' do
-    
+    expect(max_depth(root)).to eq(2)
   end
 end
