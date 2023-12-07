8e89df33b7283040fc3d19368cf21d6e337cfa54
Logan Cole <emtlogancole@gmail.com>
2023-12-07 09:20:13 -0600

    same_tree helper method for is_subtree?

--- subtree_of_another_tree/lib/is_subtree.rb
+++ subtree_of_another_tree/lib/is_subtree.rb
@@ -5,3 +5,15 @@ def is_subtree?(p, q)
   # els is_subtree?(p.right, q)
   # else false 
 end
+
+def same_tree?(p, q)
+  if p.nil? && q.nil?
+    return true
+  elsif p && q
+    p.val == q.val && 
+    same_tree?(p.left, q.left) &&
+    same_tree?(p.right, q.right)
+  else
+    false
+  end
+end
