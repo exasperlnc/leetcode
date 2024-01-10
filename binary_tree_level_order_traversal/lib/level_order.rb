5176bf5bcf6461aede4da9a4f8fda53b03f6ad54
Logan Cole <emtlogancole@gmail.com>
2024-01-10 16:42:00 -0600

    level order pseudocode

---
+++ binary_tree_level_order_traversal/lib/level_order.rb
@@ -0,0 +1,19 @@
+require 'tree_node'
+
+def level_order(root)
+  # create result array to store vals
+  
+  # save current level as cur
+
+  # put the root in cur
+
+  # while cur
+    # put the cur vals in result
+    # iterate through the nodes and put their children inside next level
+  
+    # switch cur to next
+
+  # return result
+    
+
+end
