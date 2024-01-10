7d9c9dbd4fa6119b8a28ba8c26bd9ac3f6e7fd56
Logan Cole <emtlogancole@gmail.com>
2024-01-10 16:48:46 -0600

    first pass level order, still not working

--- binary_tree_level_order_traversal/lib/level_order.rb
+++ binary_tree_level_order_traversal/lib/level_order.rb
@@ -2,19 +2,33 @@ require 'tree_node'
 
 def level_order(root)
   # create result to store vals
+  result = []
 
   # create cur array to store nodes
+  cur_level = []
+  cur_level << root
   # put root in cur
 
-  # until cur.empty? 
+  # until cur.empty?
+  until cur_level.empty?
     # save all cur vals here 
+    vals = []
     # save next level here
+    next_level = []
 
     # iterate through nodes in cur and save the vals to val, then put children in next
-    
+    cur_level.each do |node|
+      vals << node.val
+      next_level << node.left
+      next_level << node.right
+    end
     # result << vals
+    result << vals
 
     # set cur level to next
 
+    cur_level = next_level
+  end
+  return result
   # return result
 end
