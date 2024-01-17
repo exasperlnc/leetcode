6d64b9a093694a1b3a7b134f1673e4bdcc000a25
Logan Cole <emtlogancole@gmail.com>
2024-01-17 17:12:27 -0600

    second pass is_valid_bst

--- validate_bst/lib/is_valid_bst.rb
+++ validate_bst/lib/is_valid_bst.rb
@@ -1,13 +1,15 @@
 require 'tree_node'
 
 def is_valid_bst(root)
-  def valid(node, left=-(Float::INFINITY), right=(Float::INFINITY))
-    return true if !node
-    if !(left < node.val) || !(node.val < right)
-      return false
-    end
-    return (valid(node.left, left, node.val) &&
-           valid(node.right, node.val, right))
-  end
-  valid(root)
+  check_if_valid?(root)
+end
+
+def check_if_valid?(node, min: -Float::INFINITY, max: Float::INFINITY)
+  return true if node.nil? # base case: return if we go the right or left of a leaf
+
+  # tree is BST if current node val is left node, its value need to be less than all the above node, which set a max boundary by the above node val
+  # if current node is a right node, its value need to be more than the above node, which set a min boundary by the above node val
+  return false unless min < node.val && node.val < max
+  # we need to check both sub right and left tree
+  check_if_valid?(node.left, min: min, max: node.val) && check_if_valid?(node.right, min: node.val, max: max)
 end
