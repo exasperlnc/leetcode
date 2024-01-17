b07b78c4c8b38f93db0aa8e532e59593bcccdbef
Logan Cole <emtlogancole@gmail.com>
2024-01-17 15:21:12 -0600

    pseudocode good_nodes

--- unfinished_problems/count_good_nodes_in_bt/lib/good_nodes.rb
+++ unfinished_problems/count_good_nodes_in_bt/lib/good_nodes.rb
@@ -1,2 +1,14 @@
 def good_nodes(root)
+  # declare max node as or = root  
+
+  # call good nodes on left and right
+
+  # 
+
+  # +1 if the current root is >= max node 
+end
+
+def good_node(root, max=root)
+  # goodies += 1 if root >= max
+
 end
