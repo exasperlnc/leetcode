48e215e95518a9dfd38678c5ad91a4ba1133b50d
Logan Cole <emtlogancole@gmail.com>
2024-01-25 13:55:58 -0600

    kth smallest first pass

--- unfinished_problems/kth_smallest_in_bst/lib/kth_smallest.rb
+++ unfinished_problems/kth_smallest_in_bst/lib/kth_smallest.rb
@@ -1,14 +1,24 @@
 def kth_smallest(root, k)
   # store elements in a stack 
+  stack = []
   # store how many elements we have visited, first visit should be 1 (k is 1 indexed) 
+  visited = 0
   # store the current node 
+  cur = root
 
-  # while curr and stack
-    #stack.push(cur)
-    #cur = cur.left
-
-  # cur = stack.pop
-  # n+= 1
-  #if n==k
-    # return cur.val
+  # while curr or stack
+  while cur || stack
+    while cur 
+      #stack.push(cur)
+      stack << cur
+      #cur = cur.left
+      cur = cur.left
+    end
+    cur = stack.pop
+    visited += 1
+    if visited == k
+      return cur.val
+    end
+    cur = cur.right
+  end
 end
