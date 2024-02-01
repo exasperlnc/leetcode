820bbc8678ea4764ab2da59897d8f2dd12b7fd73
Logan Cole <emtlogancole@gmail.com>
2024-02-01 15:38:15 -0600

    first pass divide_array

--- divide_array_into_arrays_with_max_diff/lib/divide_array.rb
+++ divide_array_into_arrays_with_max_diff/lib/divide_array.rb
@@ -1,13 +1,26 @@
 def divide_array(nums, k)
   # make sure the array.count % 3 == 0
-  
   # else return empty array 
+  return [] if nums.count % 3 != 0
 
   # define answer array
-
+  answer = []
+  count = 0
+  current_array = []
   # put the elements in arrays of three in order? 
-
+  nums.each do |num|
   # how to double check answers are all within k? as you fill array? 
 
-  # return the new array 
+  if count < 3
+    count += 1
+    current_array << num
+  else
+    count = 0
+    answer << current_array
+    current_array = [num]
+  end
+end
+answer << current_array
+# return the new array 
+  return answer
 end
--- divide_array_into_arrays_with_max_diff/spec/divide_array_spec.rb
+++ divide_array_into_arrays_with_max_diff/spec/divide_array_spec.rb
@@ -10,13 +10,21 @@ RSpec.describe 'Divide array' do
     @nums_2 = [1,3,3,2,7,3]
     @k_2 = 3
     @out_2 = []
+
+    @nums_3 = [1]
+    @k_3 = 4
+    @out_3 = []
   end
   
   it '1' do
-    expect(divide_array(nums_1,k_1)).to eq(@out_1)
+    expect(divide_array(@nums_1,@k_1)).to eq(@out_1)
   end
 
   it '2' do
-    expect(divide_array(nums_2,k_2)).to eq(@out_2)
+    expect(divide_array(@nums_2,@k_2)).to eq(@out_2)
+  end
+
+  it '3' do
+    expect(divide_array(@nums_3,@k_3)).to eq(@out_3)
   end
 end
