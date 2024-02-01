988c26d172890469c15a08a0780c151d78a7e7cf
Logan Cole <emtlogancole@gmail.com>
2024-02-01 15:21:12 -0600

    fix final test for implement trie, still failing

--- implement_trie/spec/trie_spec.rb
+++ implement_trie/spec/trie_spec.rb
@@ -30,12 +30,12 @@ RSpec.describe Trie do
 
   xit '1' do
     trie = Trie.new()
-    trie.insert(apple)
-    expect(trie.search(apple)).to eq(true)
+    trie.insert("apple")
+    expect(trie.search("apple")).to eq(true)
 
-    expect(trie.search(app)).to eq(false)
-    expect(trie.starts_with(app)).to eq(true)
-    trie.insert(app)
-    expect(trie.search(app)).to eq(true)
+    expect(trie.search("app")).to eq(false)
+    expect(trie.starts_with("app")).to eq(true)
+    trie.insert("app")
+    expect(trie.search("app")).to eq(true)
   end
 end
