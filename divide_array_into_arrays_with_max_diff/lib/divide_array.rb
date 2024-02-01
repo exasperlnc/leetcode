51aac0f9e8cb41558c497edec03f64efdc15eebc
Logan Cole <emtlogancole@gmail.com>
2024-02-01 15:27:35 -0600

    divide_array psuedocode

--- divide_array_into_arrays_with_max_diff/lib/divide_array.rb
+++ divide_array_into_arrays_with_max_diff/lib/divide_array.rb
@@ -1,3 +1,13 @@
 def divide_array(nums, k)
+  # make sure the array.count % 3 == 0
+  
+  # else return empty array 
 
+  # define answer array
+
+  # put the elements in arrays of three in order? 
+
+  # how to double check answers are all within k? as you fill array? 
+
+  # return the new array 
 end
