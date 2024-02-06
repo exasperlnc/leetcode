27241fc182c2f7a143d657eeb4e31426fcae59b3
Logan Cole <emtlogancole@gmail.com>
2024-02-06 12:46:38 -0600

    subsets first pass; test answers need to be inverted

--- unfinished_problems/subsets/lib/subsets.rb
+++ unfinished_problems/subsets/lib/subsets.rb
@@ -1,3 +1,20 @@
 def subsets(nums)
+  result = []
 
+  dfs(0, nums, [], result)
+
+  result
+end
+
+def dfs(i, nums, subset, result)
+  if i >= nums.length
+    result << subset.clone
+    return
+  end
+  
+  subset << nums[i]
+  dfs(i + 1, nums, subset, result)
+
+  subset.pop()
+  dfs(i + 1, nums, subset, result)
 end
