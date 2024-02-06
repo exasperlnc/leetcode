b8d63746311c9e8c26537d4dfe4a1bba6137f11f
Logan Cole <emtlogancole@gmail.com>
2024-02-05 18:30:01 -0600

    test: subsets 2

---
+++ unfinished_problems/subsets_2/lib/subsets_with_dup.rb
@@ -0,0 +1,3 @@
+def subsets_with_dup(nums)
+  
+end
---
+++ unfinished_problems/subsets_2/spec/subsets_with_dup_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require './lib/subsets_with_dup'
+
+RSpec.describe 'subsets with dup' do
+  before :all do
+    @nums_1 = [1,2,2]
+    @out_1 = [[],[1],[1,2],[1,2,2],[2],[2,2]]
+
+    @nums_2 = 0
+    @out_2 = [[],[0]]
+  end
+
+  it '1' do
+    expect(subsets_with_dup(@nums_1)).to eq(@out_1)
+  end
+
+  it '2' do
+    expect(subsets_with_dup(@nums_2)).to eq(@out_2)
+  end
+end
