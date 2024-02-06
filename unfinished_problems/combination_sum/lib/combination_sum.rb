ef64b5658729a89df44b197f5adebd36779144a6
Logan Cole <emtlogancole@gmail.com>
2024-02-06 13:25:24 -0600

    first pass combination sum

--- unfinished_problems/combination_sum/lib/combination_sum.rb
+++ unfinished_problems/combination_sum/lib/combination_sum.rb
@@ -1,3 +1,25 @@
 def combination_sum(candidates, target)
+  @answers = []
+
+  # recurse some stuff
+  recurse(0, [], candidates, target, 0)
+
+  @answers
+end
+
+def recurse(i, current, candidates, target, total)
+  if total == target
+    @answers << current.dup
+    return
+  end
+  
+  if i >= candidates.length || total > target
+    return
+  end
+  
+  current.append(candidates[i])
+  recurse(i, current, candidates, target, total + candidates[i])
+  current.pop()
+  recurse(i+1, current, candidates, target, total)
 
 end
