527b5de4029ec7dbc295ffb0b966283283909c6e
Logan Cole <emtlogancole@gmail.com>
2024-02-08 17:10:32 -0600

    test: longest palindromic substring

---
+++ unfinished_problems/longest_palindromic_substring/lib/longest_palindrome.rb
@@ -0,0 +1,3 @@
+def longest_palindrome(s)
+    
+end
---
+++ unfinished_problems/longest_palindromic_substring/spec/longest_palindrome_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require './lib/longest_palindrome'
+
+RSpec.describe 'longest_palindrome' do
+  before :all do
+      @s1 = 'babad'
+      @out_1 = 'bab'
+    
+      @s2 = 'cbbd'
+      @out_2 = 'bb'
+    end
+
+    it '1' do
+      expect(longest_palindrome(@s1)).to eq(@out_1)
+    end
+
+    it '2' do
+      expect(longest_palindrome(@s2)).to eq(@out_2)
+    end
+    
+  end
