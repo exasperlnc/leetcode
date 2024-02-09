4c86bf79e484eedee0460cee21c83d6a1888ba85
Logan Cole <emtlogancole@gmail.com>
2024-02-09 13:09:56 -0600

    combination_sum 2

--- unfinished_problems/combination_sum_2/lib/combination_sum2.rb
+++ unfinished_problems/combination_sum_2/lib/combination_sum2.rb
@@ -1,3 +1,26 @@
 def combination_sum2(candidates, target)
+  @result = []
+  @candidates = candidates.sort
 
+  def backtrack(cur, pos, target)
+    @result << cur.dup if target == 0
+    return if target <= 0
+
+    prev = -1
+
+    (pos...@candidates.length).each do |i|
+      next if @candidates[i] == prev
+
+      cur.append(@candidates[i])
+
+      backtrack(cur,i+1,target-@candidates[i])
+
+      cur.pop
+
+      prev = @candidates[i]
+    end
+    
+  end
+  backtrack([], 0, target)
+  @result
 end
