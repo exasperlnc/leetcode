b56ba67c0105ee2ec10ac3d587e43bb0b0ffeea7
Logan Cole <emtlogancole@gmail.com>
2024-02-16 12:14:54 -0600

    psuedocode p1 word search

--- unfinished_problems/word_search/lib/exist.rb
+++ unfinished_problems/word_search/lib/exist.rb
@@ -1,3 +1,33 @@
 def exist(board, word)
-  
+  # check for the first letter function
+
+  # if first letter, check N
+
+  # if first letter check S
+
+  # if first letter check E 
+
+  # if first letter check W
+
 end
+
+def check_n(board, word, i)
+
+end
+
+
+def check_s(board, word, i)
+
+end
+
+
+def check_e(board, word, i)
+
+end
+
+
+def check_w(board, word, i)
+
+end
+
+
