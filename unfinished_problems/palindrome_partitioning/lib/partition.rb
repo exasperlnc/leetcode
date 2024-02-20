8d0b200c79e190b86e2fcb500f79436f1fbb7f89
Logan Cole <emtlogancole@gmail.com>
2024-02-20 16:40:19 -0600

    partitioning psuedocode

--- unfinished_problems/palindrome_partitioning/lib/partition.rb
+++ unfinished_problems/palindrome_partitioning/lib/partition.rb
@@ -1,3 +1,30 @@
 def partition(s)
-  
+  @s = s 
+  @result = []
+  @partition = []
+
+  def dfs(i)
+    # base case 
+      # add partition dup to result
+      # return
+
+    (i...@s.length).each do |j|
+      # if its a palindrome, add to partition
+      # call dfs after the end of the palindrome (j+1)
+      # pop partition
+    end
+  end
+
+  dfs(0)
+
+  @result
+end
+
+def palindrome?(s, i, j)
+  while i < j
+    return false if s[i] != s[j]
+    i += 1
+    j += 1
+  end
+  return true
 end
