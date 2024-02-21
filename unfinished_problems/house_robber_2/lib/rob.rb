493d9c98daf57f4b65bda5ba4a623153225fb7c5
Logan Cole <emtlogancole@gmail.com>
2024-02-21 15:15:52 -0600

    rob 2 psuedocode

--- unfinished_problems/house_robber_2/lib/rob.rb
+++ unfinished_problems/house_robber_2/lib/rob.rb
@@ -1,3 +1,13 @@
 def rob(nums)
-    
+  return nums.max if nums.size < 4
+  # declare dp1 with default values 0 and size of nums
+  # first one is first nums
+  # second one is max of first and second nums
+  # iterate through all but last number
+
+  # declare dp2 with default values 0 and size of nums
+  # dp2[1 is nums[1]]
+  # iterate through second an onward indexes
+
+  # return dp1-2, and dp2 -1
 end
