bed993c197857cc88b48b892992c2ac83b1746e1
Logan Cole <emtlogancole@gmail.com>
2024-02-21 16:49:20 -0600

    update test case 1, add additional failing test

--- unfinished_problems/longest_palindromic_substring/spec/longest_palindrome_spec.rb
+++ unfinished_problems/longest_palindromic_substring/spec/longest_palindrome_spec.rb
@@ -4,13 +4,16 @@ require './lib/longest_palindrome'
 RSpec.describe 'longest_palindrome' do
   before :all do
       @s1 = 'babad'
-      @out_1 = 'aba'
+      @out_1 = 'bab'
     
       @s2 = 'cbbd'
       @out_2 = 'bb'
 
       @s3 = "a"
       @out_3 = "a"
+
+      @s4 = "aacabdkacaa"
+      @out_4 = "aca"
     end
 
     it '1' do
@@ -23,5 +26,9 @@ RSpec.describe 'longest_palindrome' do
     
     it '3' do
       expect(longest_palindrome(@s3)).to eq(@out_3)
+    end
+
+    it '4' do
+      expect(longest_palindrome(@s4)).to eq(@out_4)
     end
   end
