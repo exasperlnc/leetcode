1cf5d809d3ce5024c6e3085cd57bc903e91165a5
Logan Cole <emtlogancole@gmail.com>
2024-02-21 14:36:26 -0600

    simplify psuedocode

--- unfinished_problems/house_robber/lib/rob.rb
+++ unfinished_problems/house_robber/lib/rob.rb
@@ -1,8 +1,8 @@
 def rob(nums)
- # save array with 3 bonus slots of 0
+  # save array with 3 
 
- # iterate through array with index
-  # dp[i+3] = dpi, dpi+1.max + num
+  # iterate with index
+    # dp+3 = dpi,dpi+1.max + num
 
-  # max of dp-1, dp-2
+  # dp-1,dp-2.max
 end
