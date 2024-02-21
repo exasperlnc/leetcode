f1be2248672752576e4f9f491bcf541827bb6cf9
Logan Cole <emtlogancole@gmail.com>
2024-02-21 10:41:08 -0600

    solve climbing stairs

--- unfinished_problems/climbing_stairs/lib/climb_stairs.rb
+++ unfinished_problems/climbing_stairs/lib/climb_stairs.rb
@@ -1,3 +1,19 @@
 def climb_stairs(n)
- 
+  @ans = 0
+
+  recurse(n, 0)
+
+  @ans 
+end
+
+def recurse(number, total)
+  if number == total
+    @ans += 1
+    return
+  elsif total > number
+    return
+  else 
+    recurse(number, total + 1)
+    recurse(number, total + 2)
+  end
 end
