2ee68331ad10b53e29fe1cad3b7b77307e9bc6f8
Logan Cole <emtlogancole@gmail.com>
2024-02-21 16:42:25 -0600

    first pass, brute force

--- unfinished_problems/longest_palindromic_substring/lib/longest_palindrome.rb
+++ unfinished_problems/longest_palindromic_substring/lib/longest_palindrome.rb
@@ -1,5 +1,23 @@
 def longest_palindrome(s)
+  # base case, short length? 
+  @result = ""
+  # check if current string is palindrome
+  def dfs(string)
+    # base case, string length == 1?
+    return if string.length <= 1
+    # if true, save result
+    if palindrome?(string)
+      @result = string
+      return
+    else
+      # break string down at front and back and feed it back in
+      dfs(string[0..-2])
+      dfs(string[1..-1])
+    end
+  end
+  dfs(s)
 
+  @result
 end
 
 def palindrome?(string)
