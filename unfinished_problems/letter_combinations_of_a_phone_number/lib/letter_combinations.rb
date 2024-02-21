7609a81c9ad41b9313b6aa7edc14e39fd4420b38
Logan Cole <emtlogancole@gmail.com>
2024-02-21 10:31:00 -0600

    letter combinations

--- unfinished_problems/letter_combinations_of_a_phone_number/lib/letter_combinations.rb
+++ unfinished_problems/letter_combinations_of_a_phone_number/lib/letter_combinations.rb
@@ -1,3 +1,31 @@
+$MAP = {
+    2 => %w(a b c),
+    3 => %w(d e f),
+    4 => %w(g h i),
+    5 => %w(j k l),
+    6 => %w(m n o),
+    7 => %w(p q r s),
+    8 => %w(t u v),
+    9 => %w(w x y z)    
+}
+
 def letter_combinations(digits)
- 
+  result = []
+  
+  recurse(digits, "", result, 0) unless digits.empty?
+  
+  result
+end
+
+def recurse(digits, string, result, i)
+  if digits.length == string.length
+    result << string
+    return
+  end
+
+  digit = digits[i].to_i
+
+  $MAP[digit].each do |c|
+    recurse(digits, string + c, result, i+1)
+  end
 end
